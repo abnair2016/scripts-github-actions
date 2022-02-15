e918e04b775726a4155c785e92db97576378509c
Abhilash Nair <abhilash.y.nair@mnscorp.net>
2022-02-15 17:56:06 +0000

    test github actions for acls

--- scripts/create-sa-acls.sh
+++ scripts/create-sa-acls.sh
@@ -29,24 +29,38 @@ echo "All Service Account Names: $ALL_SA_NAMES"
 ENVIRONMENT_ID=$(ccloud::get_environment_from_name $ENV_NAME)
 ccloud::use_environment $ENVIRONMENT_ID
 CLUSTERS=$(ccloud::get_clusters)
+CLUSTER_ID=""
 for cluster_id in $(echo "${CLUSTERS}" | jq -r '.[] | .id'); do
   echo "Cluster: $cluster_id"
+  echo "Service Account Names: $ALL_SA_NAMES"
+
+  while [ "$ALL_SA_NAMES" != "$sa_name" ] ;do
+    # extract the substring from start of string up to delimiter.
+    sa_name=${ALL_SA_NAMES%%;*}
+    echo "SA_NAME: $sa_name"
+    prefixed_name=$( echo "$sa_name" | cut -d - -f -3 )
+    echo "PREFIXED_NAME: $prefixed_name"
+    # delete this first "element" AND next separator, from $IN.
+    ALL_SA_NAMES="${ALL_SA_NAMES#$sa_name;}"
+    echo "ALL_SA_NAMES: $ALL_SA_NAMES"
+
+    # Gets service account resource using service account name
+    SA_RESOURCE_ID=$(ccloud::get_service_account_resource_by_name $sa_name)
+    echo "Service Account: $SA_RESOURCE_ID"
+
+    if [[ $sa_name == *-read_only ]]; then
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation READ --consumer-group '*' --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation DESCRIBE --consumer-group '*' --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation READ --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation DESCRIBE --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
+    fi
+
+    ACL_LIST=$( confluent kafka acl list --service-account $SA_RESOURCE_ID --environment ENVIRONMENT_ID --cluster $cluster_id )
+
+    echo $ACL_LIST
+
+  done
 done
-
-echo "Service Account Names: $ALL_SA_NAMES"
-while [ "$ALL_SA_NAMES" != "$sa_name" ] ;do
-  # extract the substring from start of string up to delimiter.
-  sa_name=${ALL_SA_NAMES%%;*}
-  # delete this first "element" AND next separator, from $IN.
-  ALL_SA_NAMES="${ALL_SA_NAMES#$sa_name;}"
-  echo "SA_NAME: $sa_name"
-  echo "ALL_SA_NAMES: $ALL_SA_NAMES"
-
-  # Gets service account resource using service account name
-  SA_RESOURCE_ID=$(ccloud::get_service_account_resource_by_name $sa_name)
-  echo "Service Account: $SA_RESOURCE_ID"
-done
-
 #while [ "$EMAIL_ADDRESS" != "$email_addr" ] ;do
 #  # extract the substring from start of string up to delimiter.
 #  email_addr=${EMAIL_ADDRESS%%;*}
