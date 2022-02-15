373bc84ad385331eb98ed03e8e8497a620467d9a
Abhilash Nair <abhilash.y.nair@mnscorp.net>
2022-02-15 12:26:26 +0000

    test github actions for acls

---
+++ scripts/create-sa-acls.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+source utils/helper.sh
+source utils/ccloud_library.sh
+
+## Validations to run this script
+ccloud::validate_version_ccloud_cli $CCLOUD_MIN_VERSION || exit 1
+check_jq || exit 1
+ccloud::validate_logged_in_ccloud_cli || exit 1
+
+#if [ -n "$EMAIL" ]; then
+#  ccloud::validate_required_params_for_user $EMAIL $ENVIRONMENT_NAME $ROLE || exit 1
+#  LOWER_EMAIL_ADDRESS=$(echo "$EMAIL" | awk '{print tolower($0)}')
+#  EMAIL_ADDRESS=$(echo "$LOWER_EMAIL_ADDRESS" | sed "s/marks-and-spencer.com/mnscorp.net/g")
+#  echo "$EMAIL_ADDRESS"
+#else
+#  ccloud::validate_required_params_for_user_no_email $ENVIRONMENT_NAME $ROLE || exit 1
+#  sed -i 's/marks-and-spencer.com/mnscorp.net/' users.txt
+#  EDITED=$(grep -i -o '[A-Z0-9._%+-]\+@[A-Z0-9.-]\+\.[A-Z]\{2,4\}' users.txt)
+#  EDITED_EMAIL=$(echo "$EDITED" | awk '{print tolower($0)}')
+#  EMAIL_ADDRESS="${EDITED_EMAIL//$'\n'/';'}"
+#  echo "$EMAIL_ADDRESS"
+#fi
+
+echo "Environment Name: $ENVIRONMENT_NAME"
+echo "Cluster Name: $CLUSTER_NAME"
+echo "Service Account Names: $SA_NAMES"
+
+#while [ "$EMAIL_ADDRESS" != "$email_addr" ] ;do
+#  # extract the substring from start of string up to delimiter.
+#  email_addr=${EMAIL_ADDRESS%%;*}
+#  # delete this first "element" AND next separator, from $IN.
+#  EMAIL_ADDRESS="${EMAIL_ADDRESS#$email_addr;}"
+#
+#  echo "Progressing script for email: $email_addr"
+#  ## Invites user if user does not exist, else finds the existing user
+#  EMAIL_EXISTS=$(confluent iam user invitation list -o json | jq -c -r '.[] | select(.email == "'"$email_addr"'")')
+#  if [ -z "$EMAIL_EXISTS" ]; then
+#    ccloud::invite_user $email_addr
+#  fi
+#
+#  ## Gets user resource using email address
+#  USER_RESOURCE_ID=$(ccloud::get_user_resource_by_email $email_addr)
+#  echo "User: $USER_RESOURCE_ID"
+#
+#  ## Gets environment id using environment name
+#  ENVIRONMENT_ID=$(ccloud::get_environment_from_name $ENVIRONMENT_NAME)
+#  ccloud::use_environment $ENVIRONMENT_ID
+#
+#  ## Sets role for user
+#  echo "About to set role $ROLE for User $USER_RESOURCE_ID with email $email_addr in environment $ENVIRONMENT_ID"
+#  if [ "OrganizationAdmin" == $ROLE ] || [ "MetricsViewer" == $ROLE ]; then
+#    ccloud::set_organization_level_role_for_user $USER_RESOURCE_ID $ROLE
+#  elif [ "EnvironmentAdmin" == $ROLE ]; then
+#    ccloud::set_environment_level_role_for_user $USER_RESOURCE_ID $ROLE $ENVIRONMENT_ID
+#  elif [ "CloudClusterAdmin" == $ROLE ]; then
+#    CLUSTERS=$(ccloud::get_clusters)
+#    for cluster_id in $(echo "${CLUSTERS}" | jq -r '.[] | .id'); do
+#      echo "Cluster: $cluster_id"
+#      ccloud::set_cluster_level_role_for_user $USER_RESOURCE_ID $ROLE $ENVIRONMENT_ID $cluster_id
+#    done
+#  else
+#    echo "$ROLE is not a valid role. Please retry with one of the valid roles: [OrganizationAdmin or MetricsViewer or EnvironmentAdmin or CloudClusterAdmin]"
+#  fi
+#done
