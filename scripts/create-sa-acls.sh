7cf194de4644a2f33f3b72a178f27d92db261db7
Abhilash Nair <abhilash.y.nair@mnscorp.net>
2022-02-15 17:58:40 +0000

    test github actions for acls

--- scripts/create-sa-acls.sh
+++ scripts/create-sa-acls.sh
@@ -53,6 +53,14 @@ for cluster_id in $(echo "${CLUSTERS}" | jq -r '.[] | .id'); do
       confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation DESCRIBE --consumer-group '*' --environment ENVIRONMENT_ID --cluster $cluster_id
       confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation READ --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
       confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation DESCRIBE --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
+    elif [[ $sa_name == *-read_write ]]; then
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation READ --consumer-group '*' --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation DESCRIBE --consumer-group '*' --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation READ --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation DESCRIBE --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
+      confluent kafka acl create --allow --service-account $SA_RESOURCE_ID --operation WRITE --topic $prefixed_name --prefix --environment ENVIRONMENT_ID --cluster $cluster_id
+    else
+      echo "No further ACLS need to be defined"
     fi
 
     ACL_LIST=$( confluent kafka acl list --service-account $SA_RESOURCE_ID --environment ENVIRONMENT_ID --cluster $cluster_id )
